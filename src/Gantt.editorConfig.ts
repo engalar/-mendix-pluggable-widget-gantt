5831072f10db37fcdafd32c7b31cbff90f3b299f
engalar <engalar@gmail.com>
2021-12-07 13:56:00 +0800

    rename

--- src/Gantt.editorConfig.ts
+++ src/Gantt.editorConfig.ts
@@ -1,8 +1,8 @@
 import { Properties, StructurePreviewProps, transformGroupsIntoTabs } from "./piw-utils-internal";
-import { GraphPreviewProps } from "../typings/GraphProps";
+import { GanttPreviewProps } from "../typings/GanttProps";
 
 export function getProperties(
-    values: GraphPreviewProps,
+    values: GanttPreviewProps,
     defaultProperties: Properties,
     platform: "web" | "desktop"
 ): Properties {
@@ -12,7 +12,7 @@ export function getProperties(
     }
     return defaultProperties;
 }
-export function getPreview(values: GraphPreviewProps): StructurePreviewProps | null {
+export function getPreview(values: GanttPreviewProps): StructurePreviewProps | null {
     console.log(values);
     return null;
 }
